f54bd9e5a14291ba1efecdc6f08f615a29453454
noman <nomannabil30@gmail.com>
2022-10-25 18:25:22 +0600

    login page design complete

--- src/Routers/Router.js
+++ src/Routers/Router.js
@@ -5,6 +5,7 @@ import Courses from '../Pages/Courses/Courses';
 import Error from '../Pages/Error/Error';
 import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
+import Register from '../Pages/Register/Register';
 
 const router = createBrowserRouter([
 	{
@@ -27,6 +28,10 @@ const router = createBrowserRouter([
 			{
 				path: 'login',
 				element: <Login />
+			},
+			{
+				path: 'register',
+				element: <Register />
 			}
 		]
 	}
